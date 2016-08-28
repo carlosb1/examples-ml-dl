55787f379916d4fdbb720404194387134e90a725
Carlos B <carlos.baezruiz@gmail.com>
2016-08-28 12:28:46 +0200

    Added first example for lineal regression

--- ml/test_lineal_regression.py
+++ ml/test_lineal_regression.py
@@ -13,20 +13,7 @@ data=pd.read_csv(path,header=None,names=['Population','Profit'])
 
 def computeCost(X, y, theta):
     inner = np.power(((X* theta.T) - y),2)
-    return np.sum(inner) / (2,len(X))
-
-data.insert(0,'Ones',1)
-cols = data.shape[1]
-X=data.iloc[:,0:cols-1]
-y=data.iloc[:,cols-1:cols]
-
-X=np.matrix(X.values)
-y=np.matrix(y.values)
-theta=np.matrix(np.array([0,0]))
-
-value = computeCost(X,y,theta)
-print value
-
+    return np.sum(inner) / (2*len(X))
 def gradientDescent(X,y,theta,alpha,iters):
     temp=np.matrix(np.zeros(theta.shape))
     parameters=int(theta.ravel().shape[1])
@@ -37,9 +24,29 @@ def gradientDescent(X,y,theta,alpha,iters):
         
         for j in range(parameters):
             term = np.multiply(error,X[:,j])
-            term[0,j]= theta[0,j] - ((alpha /len(X))* np.sum(term))
+            temp[0,j]= theta[0,j] - ((alpha /len(X))* np.sum(term))
         
         theta = temp
         cost[i] = computeCost(X,y,theta)
 
     return theta, cost
+
+data.insert(0,'Ones',1)
+cols = data.shape[1]
+X=data.iloc[:,0:cols-1]
+y=data.iloc[:,cols-1:cols]
+
+X=np.matrix(X.values)
+y=np.matrix(y.values)
+theta=np.matrix(np.array([0,0]))
+
+#value = computeCost(X,y,theta)
+#print value
+
+
+
+alpha=0.01
+iters=1000
+
+g, cost = gradientDescent(X, y, theta, alpha, iters)
+print g

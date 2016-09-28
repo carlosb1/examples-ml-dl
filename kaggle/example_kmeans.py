7fbf5ba293e7438ac8428e6f4353d255b7cd5bb8
carlosb <carlos.baezruiz@gmail.com>
2016-09-28 19:41:23 +0200

    Added kmeans example

---
+++ kaggle/example_kmeans.py
@@ -0,0 +1,71 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.cluster import KMeans
+import sklearn.metrics as sm
+ 
+import pandas as pd
+import numpy as np
+
+iris= datasets.load_iris()
+
+print iris.data
+print iris.feature_names
+print iris.target
+print iris.target_names
+
+# Store the inputs as a Pandas Dataframe and set the column names
+x = pd.DataFrame(iris.data)
+x.columns = ['Sepal_Length','Sepal_Width','Petal_Length','Petal_Width']
+ 
+y = pd.DataFrame(iris.target)
+y.columns = ['Targets']
+
+#Set the size of the plot
+plt.figure(figsize=(14,7))
+ 
+# Create a colormap
+colormap = np.array(['red', 'lime', 'black'])
+
+# Plot Sepal
+plt.subplot(1, 2, 1)
+plt.scatter(x.Sepal_Length, x.Sepal_Width, c=colormap[y.Targets], s=40)
+plt.title('Sepal')
+   
+plt.subplot(1, 2, 2)
+plt.scatter(x.Petal_Length, x.Petal_Width, c=colormap[y.Targets], s=40)
+plt.title('Petal')
+
+#K means cluster
+
+model = KMeans(n_clusters=3)
+model.fit(x)
+print model.labels_
+
+predY = np.choose(model.labels_, [1, 0, 2]).astype(np.int64)
+print (model.labels_)
+print (predY)
+
+#View the results
+# Set the size of the plot
+plt.figure(figsize=(14,7))
+ 
+## Create a colormap
+colormap = np.array(['red', 'lime', 'black'])
+  
+## Plot the Original Classifications
+plt.subplot(1, 2, 1)
+plt.scatter(x.Petal_Length, x.Petal_Width, c=colormap[y.Targets], s=40)
+plt.title('Real Classification')
+   
+# Plot the Models Classifications
+plt.subplot(1, 2, 2)
+plt.scatter(x.Petal_Length, x.Petal_Width, c=colormap[model.labels_], s=40)
+plt.title('K Mean Classification')
+
+#Performence sm
+print sm.accuracy_score(y,predY)
+
+#Confusion matrix
+print sm.confusion_matrix(y, predY)
+
+plt.show()

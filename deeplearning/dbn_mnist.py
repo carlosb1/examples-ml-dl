c13651bf532e2c2d6450ca6deaa44487b523d034
Carlos B <carlos.baezruiz@gmail.com>
2017-02-18 22:41:57 +0100

    Finished example for deep belief networks

--- deeplearning/dbn_mnist.py
+++ deeplearning/dbn_mnist.py
@@ -11,7 +11,7 @@ print "[X] downloading data..."
 dataset = datasets.fetch_mldata("MNIST Original")
 
 
-(trainX, testY, trainY, testY) = train_test_split(dataset.data / 255.0, dataset.target.astype('int0'), test_size=0.33)
+(trainX, testX, trainY, testY) = train_test_split(dataset.data / 255.0, dataset.target.astype('int0'), test_size=0.33)
 dbn = DBN(
         [trainX.shape[1], 300, 10], 
         learn_rates = 0.3,
@@ -23,3 +23,10 @@ dbn.fit(trainX,trainY)
 preds = dbn.predict(testX)
 print classification_report(testY,preds)
 
+for i in np.random.choice(np.arange(0,len(testY)),size=(10,)):
+    pred = dbn.predict(np.atleast_2d(testX[i]))
+
+    image = (testX[i]*255).reshape((28,28)).astype('uint8')
+    print "Actual digit is {0}, predict {1}".format(testY[i],pred[0])
+    cv2.imshow('Digit', image)
+    cv2.waitKey(0)

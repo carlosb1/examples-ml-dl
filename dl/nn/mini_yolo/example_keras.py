b75696679be944f173828644d46e3970efecb213
carlosb <carlosb@cvc.uab.es>
2017-07-04 13:57:20 +0200

    Fixed example for video. Delete video code

--- dl/nn/mini_yolo/example_keras.py
+++ dl/nn/mini_yolo/example_keras.py
@@ -70,32 +70,7 @@ out = model.predict(batch)
 
 
 boxes = yolo_net_out_to_car_boxes(out[0], threshold = 0.17)
-
-#f, (ax1,ax2) = plt.subplots(1,2,figsize=(16,6))
-
-#ax1.imshow(image)
-#ax2.imshow(draw_box(boxes,plt.imread(imagePath),[[500,1200],[300,650]]))
-#cv2.waitKey(0)
-
-#Save result image
-#cv2.imwrite('result.bmp',draw_box(boxes,plt.imread(imagePath),[[500,1280],[300,650]]))
-
-def frame_func(image):
-        crop = image[300:650,500:,:]
-        resized = cv2.resize(crop,(448,448))
-        batch = np.array([resized[:,:,0],resized[:,:,1],resized[:,:,2]])
-        batch = 2*(batch / 255.) -1
-        batch = np.expand_dims(batch, axis=0)
-        out = model.predict(batch)
-        boxes = yolo_net_out_to_car_boxes(out[0],threshold=0.17)
-        return draw_box(boxes,image[[550,1280],[300,650]])
-
-project_video_output = './project_video_output.mp4'
-clip1 = VideoFileClip('./test_resources/project_video.mp4')
-
-lane_clip = clip1.fl_image(frame_func)
-lane_clip.write_videofile(project_video_output,audio=False)
-
+cv2.imwrite('result.bmp',draw_box(boxes,plt.imread(imagePath),[[500,1280],[300,650]]))
 
 
 

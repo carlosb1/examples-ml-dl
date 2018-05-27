216ffa69a8cfa1689f3064f8155daf680575862d
Carlos B <carlos.baezruiz@gmail.com>
2018-05-27 19:18:48 +0200

    Added different type example

---
+++ dl/tensorflow/1_tensor_basics/ex2b.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+
+s = tf.constant(2.3, name='scalar', dtype=tf.float32)
+m = tf.constant([[1, 2], [3, 4]], name='matrix')
+
+with tf.Session() as sess:
+    print(sess.run(s))
+    print(sess.run(m))

5edba233e55b191a0ef47e76010e3b863d638446
Carlos B <carlos.baezruiz@gmail.com>
2018-05-27 18:39:36 +0200

    Updated exercise 1

--- dl/tensorflow/1_tensor_basics/ex1.py
+++ dl/tensorflow/1_tensor_basics/ex1.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
-a = 2
-b = 3
-c = tf.add(a, b, name='Add')
-print(c)
 
-sess = tf.Session()
-print(sess.run(c))
-sess.close()
+x = 2
+y = 3
+
+add_op = tf.add(x,y, name='Add')
+mul_op = tf.multiply(x,y, name='Multiply')
+pow_op = tf.pow(add_op, mul_op, name='Power')
+useless_op = tf.multiply(x, add_op, name='Useless')
+
+with tf.Session() as sess:
+    pow_out, useless_out = sess.run([pow_op,useless_op])
+    print([pow_out, useless_out])

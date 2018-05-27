981f1d71baf193bb15a62deaf40c0de192219726
Carlos B <carlos.baezruiz@gmail.com>
2018-05-27 19:15:55 +0200

    Added tensorflow name

--- dl/tensorflow/1_tensor_basics/ex2.py
+++ dl/tensorflow/1_tensor_basics/ex2.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 
 
-a = tf.constant(2)
-b = tf.constant(3)
+a = tf.constant(2, name='A')
+b = tf.constant(3, name='B')
 
 c = tf.add(a, b, name='Sum')
 with tf.Session() as sess:

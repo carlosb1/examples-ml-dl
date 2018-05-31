cf14269853983870631ade991cf238cdb16fedd6
Carlos B <carlos.baezruiz@gmail.com>
2018-05-31 23:52:38 +0200

    Added 0 rnn example

--- dl/tensorflow/6_RNN/0-hello-rnn.py
+++ dl/tensorflow/6_RNN/0-hello-rnn.py
@@ -30,6 +30,15 @@ with tf.variable_scope('one_cell') as scope:
 
 
 
-
+with tf.variable_scope('two_sequances') as scope:
+    # One cell RNN input_dim (4) -> output_dim (2). sequence: 5
+    hidden_size = 2
+    cell = tf.contrib.rnn.BasicRNNCell(num_units=hidden_size)
+    x_data = np.array([[h, e, l, l, o]], dtype=np.float32)
+    print(x_data.shape)
+    pp.pprint(x_data)
+    outputs, states = tf.nn.dynamic_rnn(cell, x_data, dtype=tf.float32)
+    sess.run(tf.global_variables_initializer())
+    pp.pprint(outputs.eval())
 
 

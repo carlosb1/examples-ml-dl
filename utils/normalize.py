893df5f0215796d03847de894c43da8ab6a2f51c
Carlos B <carlos.baezruiz@gmail.com>
2018-06-08 00:04:31 +0200

    Add some descriptions

--- utils/normalize.py
+++ utils/normalize.py
@@ -1,3 +1,11 @@
+"""
+Like normalization, standardization can be useful, and even required in some machine learning algorithms when your data has input values with differing scales.
+
+Standardization assumes that your observations fit a Gaussian distribution (bell curve) with a well behaved mean and standard deviation. You can still standardize your time series data if this expectation is not met, but you may not get reliable results.
+
+Standardization requires that you know or are able to accurately estimate the mean and standard deviation of observable values. You may be able to estimate these values from your training data.
+"""
+
 from pandas import Series
 from sklearn.preprocessing import MinMaxScaler
 # define contrived series
